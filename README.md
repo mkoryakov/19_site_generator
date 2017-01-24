f682ba3dc0fd732762f78307672444000bcfb24c
mkoryakov <mnkoryakov@yandex.ru>
2017-01-24 03:34:47 +0300

    Скрипт, который конвертирует markdown в html.

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # Encyclopedia
 
-[TODO. There will be project description]
+Cкрипт, который конвертирует статьи из формата markdown в html и делает из них небольшой статический сайт. Сайт будет помещён в каталог `encyclopedia`. Для улучшенного отображения страниц необходимо в каталог `encyclopedia` поместить каталог `css`.
+Результат работы скрипта доступен по адресу <https://mkoryakov.github.io/19_site_generator/encyclopedia/>
+
+# Установка зависимостей скрипта
+    $ pip install -r requirements.txt
+
+# Запуск скрипта
+    $ python md_to_html.py
 
 # Project Goals
 
--- md_to_html.py
+++ md_to_html.py
@@ -1,36 +1,70 @@
-import json
-import os.path
+from json import load
+from os import mkdir
+from os.path import exists, join, split, splitext
 from markdown import markdown
-from jinja2 import Environment, FileSystemLoader, select_autoescape
+from jinja2 import Environment, FileSystemLoader
 
 
-def load_data_from_json(file_name):
-    if not os.path.exists(file_name):
+def load_data_from_json(file_path):
+    if not exists(file_path):
         return None
-    with open(file_name) as f:
-        return json.load(f)
+    with open(file_path) as f:
+        return load(f)
 
 
-def generate_index_page(topics, articles):
-    for topic in topics[:1]:
-        html_links = [article['title'] for article in articles
-                      if topic['slug'] == article['topic']]
-    return html_links
+def get_markdown_text(file_path):
+    if not exists(file_path):
+        return None
+    with open(file_path) as f:
+        return f.read()
+
+
+def save_text_to_file(file_path, text):
+    dir_name, file_name = split(file_path)
+    if not exists(dir_name):
+        mkdir(dir_name, mode=0o755)
+    with open(file_path, 'w') as f:
+        f.write(text)
+
+
+def generate_page_from_template(template_name, data):
+    loader = FileSystemLoader('templates')
+    env = Environment(loader=loader, trim_blocks=True, lstrip_blocks=True)
+    template = env.get_template(template_name)
+    return template.render(data)
+
+
+def markdown_to_html(text):
+    return markdown(text, extensions=['codehilite'])
+
+
+def generate_site(site_name, config):
+    articles_dir = '%s/%s' % (site_name, 'articles')
+    if not exists(articles_dir):
+        mkdir(articles_dir, mode=0o755)
+    topics = config['topics']
+    articles = config['articles']
+    template = 'article.html'
+    for article in articles:
+        md_path = join('articles', article['source'])
+        md_text = get_markdown_text(md_path)
+        html_text = markdown_to_html(md_text)
+        data = {'title': article['title'], 'text': html_text}
+        page = generate_page_from_template(template, data)
+        root, ext = splitext(article['source'])
+        article['source'] = '%s%s' % (root, '.html')
+        html_path = join(articles_dir, article['source'])
+        save_text_to_file(html_path, page)
+    template = 'index.html'
+    data = {'topics': topics, 'articles': articles}
+    page = generate_page_from_template(template, data)
+    file_path = join(site_name, 'index.html')
+    save_text_to_file(file_path, page)
 
 
 if __name__ == '__main__':
+    site_name = 'encyclopedia'
+    if not exists(site_name):
+        mkdir(site_name, mode=0o755)
     config = load_data_from_json('config.json')
-    topics = config['topics']
-    articles = config['articles']
-    html_links = generate_index_page(topics, articles)
-
-    loader = FileSystemLoader('./templates')
-    env = Environment(loader=loader, trim_blocks=True, lstrip_blocks=True)
-    template = env.get_template('index.html')
-
-    # html_links = {'Main Page':'http://xgu.ru', 'Jinja':'http://xgu.ru/wiki/Jinja2'}
-    data = {'title':'Site information','header_1':'Popular pages','links':html_links}
-
-    with open("new.html", "w") as f:
-        f.write(template.render(data))
-        # f.write(markdown(open('articles/'+ articles[0]['source']).read(), extensions=['codehilite']))
+    generate_site(site_name, config)
